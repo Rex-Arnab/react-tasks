f14b9f779ecd11d2ff0d6d8063554f4213dd74fa
Arnab Biswas <khanp4397@gmail.com>
2021-11-18 16:37:57 +0530

    Removing Rate

--- task4/index.js
+++ task4/index.js
@@ -21,16 +21,13 @@
 
 // My Refactored Code
 if (province == 'ONTARIO') {
-   rate = ONTARIO_RATE;
    amt = base * ONTARIO_RATE;
 } else if ((province == 'QUEBEC') || (province == 'ALBERTA')) {
-   rate = (province == 'QUEBEC') ? QUEBEC_RATE : ALBERTA_RATE;
-   amt = base * rate;
+   amt = (province == 'QUEBEC') ? QUEBEC_RATE * base : ALBERTA_RATE * base;
    if (province == 'QUEBEC') {
        points = 2;
    }
 } else {
-   rate = 1;
    amt = base;
 }
 calc = 2 * basis(amt) + extra(amt) * 1.05;
